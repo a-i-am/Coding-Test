1599b156ae5fac35f2884d3ac61e034e198ef649
Sieun Park <70201903+a-i-am@users.noreply.github.com>
2025-12-31 15:45:12 +0900

    [Bronze V] Title: Hello World, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2557. Hello World/Hello World.cc
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    /*
+    문자열을 출력하는 문제가 나오면, 직접 타이핑하기보다 
+    문제 설명에 있는 예제 출력을 복사해 넣자.
+    */
+    cout << "Hello World!";
+    return 0;
+    
+}
