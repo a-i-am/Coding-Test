c1040b834701c3575af4b7fcbe2793db188dd55b
Sieun Park <70201903+a-i-am@users.noreply.github.com>
2026-01-28 22:57:16 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 200 ms, Memory: 5928 KB -BaekjoonHub

--- 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.cc
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.cc
@@ -2,24 +2,24 @@
 #include <algorithm>
 using namespace std;
 
-int nums[1000001];
-
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
 
     int N;
     cin >> N;
-
+    int* nums = new int[N];
+    
     for (int i = 0; i < N; i++) {
         cin >> nums[i];
     }
-
+    
     sort(nums, nums + N);
-
+    
     for (int i = 0; i < N; i++) {
         cout << nums[i] << "\n";
     }
-
+    
+    delete[] nums;
     return 0;
 }
