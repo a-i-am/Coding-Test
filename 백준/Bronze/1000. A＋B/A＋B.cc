d79db3dcb79b6fc9d3e442ea6a1aa18d2f13d8dd
Sieun Park <70201903+a-i-am@users.noreply.github.com>
2026-03-05 11:01:55 +0900

    [Bronze V] Title: A+B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.cc
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int numA = 0;
+    int numB = 0;
+    
+    cin >> numA >> numB;
+    cout << numA + numB << endl;
+    
+    return 0;
+}
